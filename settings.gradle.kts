fbf3d2c1b7923090fdad5e34d635b73c9fa1b416
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2025-06-14 18:12:28 +1000

    Initial project setup

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "kotlin-usb-client-library"
+include(":library")
