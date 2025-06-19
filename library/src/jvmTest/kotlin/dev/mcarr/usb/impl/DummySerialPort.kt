420ccfc26a968f70ba63b05ac50c5257ea3f8436
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2025-06-19 18:42:31 +1000

    Implemented unit tests

---
+++ library/src/jvmTest/kotlin/dev/mcarr/usb/impl/DummySerialPort.kt
@@ -0,0 +1,175 @@
+package dev.mcarr.usb.impl
+
+import dev.mcarr.usb.abstracts.AbstractSerialPortWrapper
+import kotlinx.coroutines.delay
+import java.nio.ByteBuffer
+
+/**
+ * Dummy implementation of a serial port.
+ *
+ * Used for unit testing purposes.
+ * */
+class DummySerialPort : AbstractSerialPortWrapper() {
+
+    /**
+     * Keeps track of whether the port is "open" or not, since
+     * there's no actual port to check.
+     * */
+    var socketIsOpen = false
+
+    /**
+     * Delay in milliseconds between each read or write to the
+     * socket.
+     *
+     * This is used to simulate the bitrate or latency of the
+     * serial port read/write operations, and for testing
+     * timeouts.
+     * */
+    var emitMillis: Long = 0
+
+    /**
+     * Bytes which this serial port will "send" to the client which
+     * tries to read from it.
+     * */
+    var bytesToSend = ByteBuffer.allocate(0)
+
+    /**
+     * Bytes which this serial port has received from a client which
+     * has written bytes to it.
+     * */
+    var bytesReceived = ByteBuffer.allocate(0)
+
+    /**
+     * Prepares this dummy port for an impending read operation
+     * by a client.
+     *
+     * Fills the dummy port with data for the client to read,
+     * and specifies how long the dummy port should wait between
+     * each byte before sending another to the client.
+     *
+     * @param bytesToSend Bytes for the dummy port to send to
+     * the next client which reads from it
+     * @param emitMillis Delay in milliseconds between each byte
+     * being received the next time a client tries to read from
+     * this dummy port
+     * */
+    fun prepareRead(bytesToSend: ByteArray, emitMillis: Long){
+        this.bytesToSend = ByteBuffer.wrap(bytesToSend)
+        this.emitMillis = emitMillis
+    }
+
+    /**
+     * Prepares this dummy port for an impending write operation
+     * by a client.
+     *
+     * Tells the dummy port how much data to expect from the client,
+     * and specifies how long the dummy port should wait between
+     * each byte before receiving another from the client.
+     *
+     * @param bytesToReceive Number of bytes which the dummy port
+     * should expect from the next client which writes to it
+     * @param emitMillis Delay in milliseconds between each byte
+     * being written the next time a client tries to write to
+     * this dummy port
+     * */
+    fun prepareWrite(bytesToReceive: Int, emitMillis: Long){
+        this.bytesReceived = ByteBuffer.allocate(bytesToReceive)
+        this.emitMillis = emitMillis
+    }
+
+    override suspend fun readBytes(buffer: ByteArray): Int {
+
+        var i = 0
+
+        // Loop over the buffer
+        while (i < buffer.size) {
+
+            // Wait the specified number of millis before each iteration
+            delay(emitMillis)
+
+            // If there are still bytes left to send to the client, then
+            // put a byte in the output buffer and continue the loop
+            if (bytesToSend.hasRemaining()) {
+                buffer.set(i, bytesToSend.get())
+                i++
+            }else{
+
+                // If there are no more bytes left, enforce a small delay
+                // for every iteration until the timeout is reached.
+                // If no delay is specified, wait for 10ms each loop iteration.
+                //
+                // Once the timeout is reached, the enclosing withTimeout block
+                // will throw an exception.
+                //
+                // This is done to mimic the behavior of a real serial port,
+                // which might stop producing data and wait endlessly.
+                if (emitMillis == 0L){
+                    delay(10)
+                }
+
+            }
+        }
+
+        // If we manage to read the expected amount of data into the buffer,
+        // return the number of bytes read into it
+        return i
+
+    }
+
+    override suspend fun writeBytes(buffer: ByteArray): Int {
+
+        var i = 0
+
+        // Loop over the buffer
+        while (i < buffer.size) {
+
+            // Wait the specified number of millis before each iteration
+            delay(emitMillis)
+
+            // If the port hasn't reached its maximum capacity, write to it
+            if (i < bytesReceived.capacity()) {
+                bytesReceived.put(buffer[i])
+                i++
+            }else{
+
+                // If the port has reached its maximum capacity, enforce a small delay
+                // for every iteration until the timeout is reached.
+                // If no delay is specified, wait for 10ms each loop iteration.
+                //
+                // Once the timeout is reached, the enclosing withTimeout block
+                // will throw an exception.
+                //
+                // This is done to mimic the behavior of a real serial port,
+                // which might stop reading data and wait endlessly.
+                if (emitMillis == 0L){
+                    delay(10)
+                }
+
+            }
+        }
+
+        // If we manage to write the expected amount of data to the port,
+        // return the number of bytes written
+        return i
+
+    }
+
+    override fun setTimeout() {
+        // Do nothing.
+        // We don't need to override the "default" port timeout behavior,
+        // since there isn't one.
+    }
+
+    override fun open() {
+        socketIsOpen = true
+    }
+
+    override fun close() {
+        socketIsOpen = false
+    }
+
+    override fun isOpen(): Boolean {
+        return socketIsOpen
+    }
+
+}
---
+++ library/src/jvmTest/kotlin/dev/mcarr/usb/impl/DummySerialPortTest.kt
@@ -0,0 +1,265 @@
+package dev.mcarr.usb.impl
+
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import java.util.concurrent.TimeoutException
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNotEquals
+import kotlin.test.assertTrue
+
+/**
+ * Unit tests for the dummy serial port implementation.
+ *
+ * Mocks the behavior of a real serial port and
+ * runs logic tests based on the expected results.
+ *
+ * This is done because we can't reliably test a "real"
+ * USB device inside of an automated unit test.
+ *
+ * @see DummySerialPort
+ * */
+class DummySerialPortTest {
+
+    lateinit var port: DummySerialPort
+
+    @Before
+    fun setup(){
+        port = DummySerialPort()
+    }
+
+    /**
+     * Test if the open and close functions do what we
+     * expect them to.
+     * */
+    @Test
+    fun testOpenAndClose(){
+
+        // Port is closed by default
+        assertFalse(port.isOpen())
+
+        // Open the port
+        port.open()
+        assertTrue(port.isOpen())
+
+        // Close the port
+        port.close()
+        assertFalse(port.isOpen())
+
+    }
+
+    /**
+     * Test if the use function opens and closes
+     * the port automatically.
+     * */
+    @Test
+    fun testUse(){
+
+        // Port is closed by default
+        assertFalse(port.isOpen())
+
+        // Port is open inside of the use block
+        port.use {
+            assertTrue(it.isOpen())
+        }
+
+        // Port is closed after the use block completes
+        assertFalse(port.isOpen())
+
+        // Open the port with a use block, and throw an exception
+        // while the port is in use
+        try {
+            port.use {
+                throw Exception("Intentionally triggered exception")
+            }
+        }catch (e: Exception){
+            //
+        }
+
+        // Confirm that the use block has closed the port even though an
+        // exception was thrown
+        assertFalse(port.isOpen())
+
+    }
+
+    /**
+     * Try to read data from a dummy usb device.
+     *
+     * The dummy device is preloaded with data to be "read"
+     * by this test.
+     * We then read that data from the device and check if
+     * it matches our expectations.
+     * */
+    @Test
+    fun testRead() {
+
+        // Set the read timeout to 100ms
+        port.setTimeouts(100, 0)
+
+        // Empty read
+        runTest {
+            val bytesRead = port.read(0)
+            assertEquals(0, bytesRead.size)
+        }
+
+        // Partial read
+        runTest {
+            val payload = byteArrayOf(0, 1, 2, 3)
+            port.prepareRead(payload, 10)
+            val bytesRead = port.read(2)
+            assertEquals(2, bytesRead.size)
+            assertFalse(payload.contentEquals(bytesRead))
+        }
+
+        // Full read
+        runTest {
+            val payload = byteArrayOf(0, 1, 2, 3)
+            port.prepareRead(payload, 10)
+            val bytesRead = port.read(4)
+            assertEquals(4, bytesRead.size)
+            assertTrue(payload.contentEquals(bytesRead))
+        }
+
+        // Incomplete read
+        // Try to read 5 bytes, but only 4 available
+        runTest {
+            val payload = byteArrayOf(0, 1, 2, 3)
+            port.prepareRead(payload, 10)
+            var timedOut = false
+            try {
+                port.read(5)
+            } catch (e: TimeoutCancellationException) {
+                // Timeout
+                timedOut = true
+            }
+            assertTrue(timedOut)
+        }
+
+        // Read timeout
+        // Has enough data, but it takes too long to read
+        // because of the increased emit time
+        runTest {
+            val payload = byteArrayOf(0, 1, 2, 3)
+            port.prepareRead(payload, 50)
+            var timedOut = false
+            try {
+                port.read(4)
+            } catch (e: TimeoutCancellationException) {
+                // Timeout
+                timedOut = true
+            }
+            assertTrue(timedOut)
+        }
+
+        // Read timeout
+        // Has enough data, but it takes too long to read
+        // because of the increased data size
+        runTest {
+            val payload = byteArrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+            port.prepareRead(payload, 10)
+            var timedOut = false
+            try {
+                port.read(11)
+            } catch (e: TimeoutCancellationException) {
+                // Timeout
+                timedOut = true
+            }
+            assertTrue(timedOut)
+        }
+
+    }
+
+    /**
+     * Try to write data to a dummy usb device.
+     *
+     * The dummy device is initialized with a fake limit and
+     * read timeout, so it's possible to feed it too much
+     * data, or for the operation to take too long.
+     *
+     * This is to simulate how a read device might act.
+     * ie. It might refuse to read any more data, or it might
+     * process the data as it reads and wind up taking too long.
+     * */
+    @Test
+    fun testWrite() {
+
+        port.setTimeouts(0, 100)
+
+        // Empty write
+        runTest {
+            val written = port.write(byteArrayOf())
+            assertEquals(0, written)
+        }
+
+        // Partial write
+        // Try to write 5 bytes, but only 4 available
+        runTest {
+            val payload = byteArrayOf(0, 1, 2, 3)
+            port.prepareWrite(5, 10)
+            val written = port.write(payload)
+            assertEquals(4, written)
+            assertFalse(payload.contentEquals(port.bytesReceived.array()))
+        }
+
+        // Full write
+        runTest {
+            val payload = byteArrayOf(0, 1, 2, 3)
+            port.prepareWrite(4, 10)
+            val written = port.write(payload)
+            assertEquals(4, written)
+            assertTrue(payload.contentEquals(port.bytesReceived.array()))
+        }
+
+        // Incomplete write
+        // Device is only willing to accept the first 2 bytes,
+        // but we're trying to write 4
+        runTest {
+            val payload = byteArrayOf(0, 1, 2, 3)
+            port.prepareWrite(2, 10)
+            var timedOut = false
+            try {
+                port.write(payload)
+            } catch (e: TimeoutCancellationException) {
+                // Timeout
+                timedOut = true
+            }
+            assertTrue(timedOut)
+        }
+
+        // Write timeout
+        // Has enough data, but it takes too long to write
+        // because of the increased emit time
+        runTest {
+            val payload = byteArrayOf(0, 1, 2, 3)
+            port.prepareWrite(4, 50)
+            var timedOut = false
+            try {
+                port.write(payload)
+            } catch (e: TimeoutCancellationException) {
+                // Timeout
+                timedOut = true
+            }
+            assertTrue(timedOut)
+        }
+
+        // Write timeout
+        // Has enough data, but it takes too long to write
+        // because of the increased data size
+        runTest {
+            val payload = byteArrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+            port.prepareWrite(11, 10)
+            var timedOut = false
+            try {
+                port.write(payload)
+            } catch (e: TimeoutCancellationException) {
+                // Timeout
+                timedOut = true
+            }
+            assertTrue(timedOut)
+        }
+
+    }
+
+}
