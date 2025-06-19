93cd2e6df46fb61fde05a696c482d8c5e8cac331
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2025-06-19 18:35:40 +1000

    Added classes implementing the serial port interfaces using USB Serial For Android library

---
+++ library/src/androidMain/kotlin/dev/mcarr/usb/impl/UsfaPortList.kt
@@ -0,0 +1,25 @@
+package dev.mcarr.usb.impl
+
+import android.content.Context
+import android.hardware.usb.UsbManager
+import com.hoho.android.usbserial.driver.UsbSerialProber
+import dev.mcarr.usb.interfaces.ISerialPortList
+import dev.mcarr.usb.interfaces.ISerialPortWrapper
+
+/**
+ * "Usb Serial For Android" port-listing class.
+ *
+ * @param c Context
+ * */
+class UsfaPortList(
+    val c: Context
+) : ISerialPortList {
+
+    override fun get(): List<ISerialPortWrapper> {
+        val manager = c.getSystemService(Context.USB_SERVICE) as UsbManager
+        return UsbSerialProber.getDefaultProber()
+            .findAllDrivers(manager)
+            .map { UsfaPortWrapper(c, it) }
+    }
+
+}
---
+++ library/src/androidMain/kotlin/dev/mcarr/usb/impl/UsfaPortWrapper.kt
@@ -0,0 +1,69 @@
+package dev.mcarr.usb.impl
+
+import android.content.Context
+import android.hardware.usb.UsbManager
+import com.hoho.android.usbserial.driver.UsbSerialDriver
+import com.hoho.android.usbserial.driver.UsbSerialPort
+import dev.mcarr.usb.abstracts.AbstractSerialPortWrapper
+
+/**
+ * "Usb Serial For Android" port wrapper class.
+ *
+ * @param c Context
+ * @param driver USB driver/device to interact with
+ * */
+class UsfaPortWrapper(
+    c: Context,
+    val driver: UsbSerialDriver
+) : AbstractSerialPortWrapper() {
+
+    val manager = c.getSystemService(Context.USB_SERVICE) as UsbManager
+    lateinit var port: UsbSerialPort
+
+    override suspend fun readBytes(buffer: ByteArray): Int {
+        val timeout = 0
+        return port.read(buffer, timeout)
+    }
+
+    override suspend fun writeBytes(buffer: ByteArray): Int {
+        val timeout = 0
+        port.write(buffer, timeout)
+        return buffer.size
+    }
+
+    override fun setTimeout() {
+        // Unnecessary, since the timeout is set on each
+        // individual read and write
+    }
+
+    override fun open() {
+
+        val connection = manager.openDevice(driver.device) ?: throw Exception("Failed to open device")
+
+        // Most devices have just one port (port 0), so let's grab the first port
+        // TODO At some point this could be made configurable
+        port = driver.ports.firstOrNull() ?: throw Exception("Failed to open device")
+        port.open(connection)
+        //port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE)
+
+    }
+
+    override fun close() {
+        port.close()
+    }
+
+    override fun isOpen(): Boolean {
+        return port.isOpen
+    }
+
+    override fun setBaudRate(rate: Int) {
+        // TODO check what these values actually are
+        port.setParameters(rate, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE)
+    }
+
+    override fun bytesAvailable(): Int {
+        // TODO find if it has this method
+        return 0
+    }
+
+}
